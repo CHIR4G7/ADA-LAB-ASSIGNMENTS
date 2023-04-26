040fb4ee32de4104b7005ba2b8e2319f7bc7a63e
CHIR4G7 <93506828+CHIR4G7@users.noreply.github.com>
2023-04-26 23:27:22 +0530

    Update asg_2_q_2.cpp

--- ASSIGNMENT 2/asg_2_q_2.cpp
+++ ASSIGNMENT 2/asg_2_q_2.cpp
@@ -7,11 +7,17 @@ using namespace std;
 
 int power(int n,int k)
 {
+    //BASE CASE - if k==0 this will return 1 becuse 2 raised to power 0 willl be 1 itself.
     if(k==0)
     {
         return 1;
     }
-    return n*power(n,k-1);
+    
+    //SMALL CALCULATIONS
+    int small = power(n,k-1);
+    
+    //RECURSCIVE CALL
+    return n*small;
 }
  
 int main()
