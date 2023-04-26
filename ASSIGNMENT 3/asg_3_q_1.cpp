eac775d561f82796a99afc38579b2d67c8509172
CHIR4G7 <93506828+CHIR4G7@users.noreply.github.com>
2023-04-26 23:36:45 +0530

    Update asg_3_q_1.cpp

--- ASSIGNMENT 3/asg_3_q_1.cpp
+++ ASSIGNMENT 3/asg_3_q_1.cpp
@@ -6,23 +6,45 @@
 #include <stack>
 using namespace std;
 
+
+
 void reversestack(stack<int> &st)
 {
+    //base case - when the stack gets empty we return.
     if(st.empty()==true)
     {
         return;
     }
+    
+    //at every recursive call we store the top elemtn in a variable seperately.
     int a = st.top();
     st.pop();
+  
+    //recursicely call until stack gets empty.
     reversestack(st);
+    
+    //onece its empty when we are returning we will put back the element storedd in the variable and hence the stack would be reversed.
     st.push(a);
 }
+
+
+//print the stack
+void printStack(stack<int> &st)
+{
+     while(st.size()!=0)
+    {
+        cout << st.top();
+        st.pop();
+    }
+}
+
  
 int main()
 {
     int n;
     cin >> n;
 
+    //storing elements in a stack.
     stack<int> st;
     for(int i=0;i<n;i++)
     {
@@ -30,12 +52,12 @@ int main()
         cin >> a;
         st.push(a);
     }
-    while(st.size()!=0)
-    {
-        cout << st.top();
-        st.pop();
-    }
-    
+   
+    printStack(st);//before reversing
     reversestack(st);
+    printStack(st);//after reversing
    
 }
+
+//TIME COMPLEXITY - O(N)
+//because we are travelling the stack of n elements which will take O(N) and push and pop operations take O(1) time only.
